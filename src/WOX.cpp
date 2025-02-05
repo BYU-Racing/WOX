f7ae4e578f609bba5e6755318f17e2a5171060d4
btoone <braden@toonetown.com>
2025-02-04 23:49:52 -0700

    Replaced previous RPM calculations with a reciprocal method (time between pulses)

--- src/WOX.cpp
+++ src/WOX.cpp
@@ -4,48 +4,77 @@
 #include <Reserved.h>
 #include <BufferPacker.h>
 
-WOX::WOX(const uint8_t wheelId): wheelId(wheelId)
-{
-}
+#define MAGNET_COUNT 4
+#define MIN_PULSE_INTERVAL 5
+#define MS_PER_MIN 60000.0f
+#define MS_PER_MAGNET MS_PER_MIN / MAGNET_COUNT
+// RPM takes (DECAY_INTERVAL_FACTOR x last pulse interval) milliseconds to decay to 0
+#define DECAY_INTERVAL_FACTOR 2.0f
 
 void WOX::boot(FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16>* can)
 {
     this->can = can;
 }
 
+
 void WOX::seen()
 {
-    seenHoles++;
-}
-
-void WOX::reset()
-{
-    seenHoles = 0;
+    if (const uint32_t now = millis(); now - lastPulse_ms > MIN_PULSE_INTERVAL)
+    {
+        // Prevent the sensor from registering more than 3000 RPM
+        lastPulse_ms = currentPulse_ms;
+        currentPulse_ms = now;
+        pulseInterval_ms = currentPulse_ms - lastPulse_ms;
+        pulseFlag = true;
+    }
 }
 
 void WOX::calculateRPM()
 {
-    // TODO: Correct count
-    // How to handle reverse?
-    rpm = seenHoles / 12;
-}
-
-void WOX::run()
-{
-    if (millis() - lastCheck >= interval)
+    const uint32_t now = millis();
+    if (pulseFlag)
     {
-        calculateRPM();
-        sendCAN();
-        reset();
+        // Process most recent detection
+        if (pulseInterval_ms > 0)
+        {
+            rpm = MS_PER_MAGNET / pulseInterval_ms;
+        }
+        pulseFlag = false;
+        lastDecay_ms = now;
+        return;
     }
+
+    if (rpm == 0) return;
+
+    // Decay RPM
+    const float decayRate = rpm / (DECAY_INTERVAL_FACTOR * pulseInterval_ms);
+    const float decay = decayRate * (now - lastDecay_ms);
+    if (rpm > decay && rpm - decay > 0)
+    {
+        rpm -= decay;
+    } else
+    {
+        rpm = 0;
+    }
+    lastDecay_ms = now;
 }
 
 void WOX::sendCAN()
 {
     msg.id = TireRPMId;
     msg.buf[0] = wheelId;
-    BufferPacker<3> packer(msg.buf);
+    BufferPacker<sizeof(uint8_t) + sizeof(float)> packer(msg.buf);
     packer.skip<uint8_t>();
     packer.pack(rpm);
     can->write(msg);
+}
+
+void WOX::run()
+{
+    if (millis() - lastSent_ms >= sendInterval_ms)
+    {
+        lastSent_ms = millis();
+        calculateRPM();
+        sendCAN();
+    }
 }
